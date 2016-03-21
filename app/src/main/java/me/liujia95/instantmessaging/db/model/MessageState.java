749ff0e2f75cf5adf4d8567e7d13cf67ca6057f1
liujia95 <liujia95me@126.com>
2016-03-21 20:02:02 +0800

    add：增加/删除联系人的列表刷新

--- app/src/main/java/me/liujia95/instantmessaging/db/model/MessageState.java
+++
@@ -1,9 +0,0 @@
-package me.liujia95.instantmessaging.db.model;
-
-/**
- * Created by Administrator on 2016/2/29 16:40.
- */
-public enum MessageState {
-    //未送达 --> 已送达 --> 未读 --> 已读
-    READ, UNREAD, UNDELIVERED, DELIVERED
-}
--- app/src/main/java/me/liujia95/instantmessaging/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package me.liujia95.instantmessaging.utils;
-
-/**
- * Created by Administrator on 2016/2/26 16:43.
- */
-public interface Constants {
-    int DEBUGLEVEL = LogUtils.LEVEL_ALL;
-}
